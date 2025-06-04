42fdda309a1cf56201580245f98972f75930eea3
Brendan Bessman <bdbessman@gmail.com>
2025-06-03 22:19:50 -0700

    initial commit

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,37 @@
+
+import { FaFacebookF, FaInstagram, FaMailBulk, FaMailchimp, FaYoutube } from 'react-icons/fa';
+import { Button } from "@mui/material"; 
+import { Fa0, FaLetterboxd, FaMessage, FaSquareLetterboxd } from 'react-icons/fa6';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="background" />
+
+      <div className="overlay">
+        <nav className="navbar">
+          <div className="nav-left">
+            <a href="#" className="active">Home</a>
+            <a href="#">Music</a>
+            <a href="#">Contact</a>
+          </div>
+          <div className="nav-right">
+            <a href="#"><FaFacebookF /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaYoutube /></a>
+          </div>
+        </nav>
+
+        <div className="hero">
+          <>999MB</>
+          <p>Local Band in Bend Oregon</p>
+        </div>
+        <div className="contact-button">
+            <a className="contact" href="mailto:contact999mb@gmail.com"><FaMessage/>{" "}CONTACT US</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
